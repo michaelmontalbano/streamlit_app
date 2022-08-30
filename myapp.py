b616af55f1bb0560123af1e9477748aa9b73142e
michaelmontalbano <michael.montalbano@ou.edu>
2022-08-30 14:10:42 -0500

    Adding an app to the repository, ;)

--- myapp.py
+++ myapp.py
@@ -12,38 +12,52 @@ import metrics
 
 # 2: loss-mse_dataset-shave_L2-0.01_drop-0.1_junct-Add_filters-64f128_act-lrelu_cut-30_transpose-1_gain-0.0_bias-0.0_init-normal_variant-unetpp_block-vanilla_exp_index-7_kernel-3_out-act-relu_results.pkl
 
-models = ['1','6','7','10','11']
+models = ['shave', 'eff66']
+model_numbers = [0, 1, 2, 3, 4]
 datasets = ['all','severe','sig-severe']
 st.title('Model Prediction Metrics')
 model = st.radio("Pick a model.", models)
+model_number = st.radio("Pick a model number.", model_numbers)
 dataset = st.radio("Pick a dataset.", datasets)
-cutoff = st.number_input("Pick a cutoff value")
-multiplier = st.number_input('Pick a multiplier for beta')
-k_pct = st.number_input('Pick a percentage for k')
-beta = 12960000*multiplier
-number = st.number_input("Pick a sample number (0-939)", 0, 939)
+
+if model == 'shave':
+    y_true = load('data/y_true.npy').squeeze()
+    y_pred = load(f'data/y_pred_{model_number}.npy').squeeze()
+
+if model == 'eff66':
+    y_true = load('data/y_true_66.npy').squeeze()
+    y_pred = load('data/y_pred_66.npy').squeeze()
+    indices = range(len(y_true))
+    y_true = y_true[indices,:,:]
+    y_pred = y_pred[indices,:,:]
+
+indices = range(len(y_true))
+length = len(indices)
+
+if dataset == 'severe':
+    # keep only images where each image in y_true has max above 25
+    indices = [x for x,y in zip(range(len(y_true)),y_true) if np.max(y) > 25]
+if dataset == 'sig-severe':
+    indices = [x for x,y in zip(range(len(y_true)),y_true) if np.max(y) > 50]
+
+y_true = y_true[indices,:,:]
+y_pred = y_pred[indices,:,:]
 
 
-coeficients = st.text_input('Enter the coefficients')
+# cutoff = st.number_input("Pick a cutoff value")
+# multiplier = st.number_input('Pick a multiplier for beta')
+# k_pct = st.number_input('Pick a percentage for k')
+# beta = 12960000*multiplier
+number = st.number_input(f"Pick a sample number (0-{length})", 0, length)
+
+
+# coeficients = st.text_input('Enter the coefficients')
 
 
 # convert coefficients to list of comma separated floats
-coeficients = coeficients.split(',')
-cCSI, cHaus, cPHDK, cGbeta, cDelta, cG, cZhu, cFA, cMiss = [float(i) for i in coeficients]
-if int(model) < 11:
-    y_true = load('data/y_true.npy').squeeze()
-    y_pred = load(f'data/y_pred_{model}.npy').squeeze()
-    indices = range(len(y_true))
-    if dataset == 'severe':
-        # keep only images where each image in y_true has max above 25
-        indices = [x for x,y in zip(range(len(y_true)),y_true) if np.max(y) > 25]
-    if dataset == 'sig-severe':
-        indices = [x for x,y in zip(range(len(y_true)),y_true) if np.max(y) > 50]
-    y_true = y_true[indices,:,:]
-    y_pred = y_pred[indices,:,:]
-else:
-    y_true = load('data/y_true_24f48f96.npy').squeeze()
-    y_pred = load('data/y_pred_24f48f96.npy').squeeze()
+# coeficients = coeficients.split(',')
+# cCSI, cHaus, cPHDK, cGbeta, cDelta, cG, cZhu, cFA, cMiss = [float(i) for i in coeficients]
+
 
 
 
@@ -83,65 +97,65 @@ st.pyplot(f, size = 100)
 
 
 
-far_funct = metrics.FAR(cutoff)
-pod_funct = metrics.POD(cutoff)
+# far_funct = metrics.FAR(cutoff)
+# pod_funct = metrics.POD(cutoff)
 
-# binarize A and B
-A = np.where(img_true > cutoff, 1, 0)
-B = np.where(img_pred > cutoff, 1, 0)
-min_nonzero = min(A.sum(), B.sum())
-# get mean squared error of img_true and img_pred
-mse = np.mean((img_true - img_pred)**2)
-if min_nonzero > 1:
-    far = far_funct(A,B)
-    pod = pod_funct(A,B)
-    mindists_AB = np.asarray(metrics.min_dists(img_true, img_pred, cutoff))
-    mindists_BA = np.asarray(metrics.min_dists(img_pred, img_true, cutoff))
-    hausdorf_distance = metrics.hausdorf(mindists_AB, mindists_BA)
-    phdk_distance = metrics.PHDK(mindists_AB, mindists_BA, k_pct)
-    gbeta = metrics.G_beta(A, B, mindists_AB, mindists_BA, beta)
-    delta = metrics.delta(A, B)
-    G = metrics.G(A, B, mindists_AB, mindists_BA, beta)
-    zhulak = metrics.zhulak(A, B, mindists_AB)
-    medFA = np.mean(mindists_BA)
-    medMiss = np.mean(mindists_AB)
-    #podVilage, farVilage = metrics.neighborhood_stats_by_image(A, B, 10)
-else:
+# # binarize A and B
+# A = np.where(img_true > cutoff, 1, 0)
+# B = np.where(img_pred > cutoff, 1, 0)
+# min_nonzero = min(A.sum(), B.sum())
+# # get mean squared error of img_true and img_pred
+# mse = np.mean((img_true - img_pred)**2)
+# if min_nonzero > 1:
+#     far = far_funct(A,B)
+#     pod = pod_funct(A,B)
+#     mindists_AB = np.asarray(metrics.min_dists(img_true, img_pred, cutoff))
+#     mindists_BA = np.asarray(metrics.min_dists(img_pred, img_true, cutoff))
+#     hausdorf_distance = metrics.hausdorf(mindists_AB, mindists_BA)
+#     phdk_distance = metrics.PHDK(mindists_AB, mindists_BA, k_pct)
+#     gbeta = metrics.G_beta(A, B, mindists_AB, mindists_BA, beta)
+#     delta = metrics.delta(A, B)
+#     G = metrics.G(A, B, mindists_AB, mindists_BA, beta)
+#     zhulak = metrics.zhulak(A, B, mindists_AB)
+#     medFA = np.mean(mindists_BA)
+#     medMiss = np.mean(mindists_AB)
+#     #podVilage, farVilage = metrics.neighborhood_stats_by_image(A, B, 10)
+# else:
 
-    far = 0
-    pod = 0
-    hausdorf_distance = -99
-    phdk_distance = -99
-    gbeta = -99
-    delta = -99
-    G = -99
-    zhulak = 60
+#     far = 0
+#     pod = 0
+#     hausdorf_distance = -99
+#     phdk_distance = -99
+#     gbeta = -99
+#     delta = -99
+#     G = -99
+#     zhulak = 60
 
-metrics_dict = {'MSE': mse, 'FAR': [far], 'POD': [pod], 'Hausdorff': [hausdorf_distance], 'PHDK': [phdk_distance], 'Gbeta': [gbeta], 'G': [G], 'delta': [delta]}
+# metrics_dict = {'MSE': mse, 'FAR': [far], 'POD': [pod], 'Hausdorff': [hausdorf_distance], 'PHDK': [phdk_distance], 'Gbeta': [gbeta], 'G': [G], 'delta': [delta]}
 
-metrics_dict_2 = {'zhulak': [zhulak], 'medFA': [medFA], 'medMiss': [medMiss]}
+# metrics_dict_2 = {'zhulak': [zhulak], 'medFA': [medFA], 'medMiss': [medMiss]}
 
-# loss_1 = mse + c1*hausdorf_distance + c2*phdk_distance + c3*-1*gbeta + c4*delta + c5*G + c6*zhulak + c7*medFA + c8*medMiss
-# loss_2 = mse + (far-pod)*mse*c0 + c1*hausdorf_distance + c2*phdk_distance + c3*gbeta + c4*delta + c5*G + c6*zhulak + c7*medFA + c8*medMiss
-# loss_3 = mse + mse*c1*hausdorf_distance + c2*mse*phdk_distance - c3*gbeta*mse + c4*delta*mse + c6*zhulak + c7*medFA + c8*medMiss
+# # loss_1 = mse + c1*hausdorf_distance + c2*phdk_distance + c3*-1*gbeta + c4*delta + c5*G + c6*zhulak + c7*medFA + c8*medMiss
+# # loss_2 = mse + (far-pod)*mse*c0 + c1*hausdorf_distance + c2*phdk_distance + c3*gbeta + c4*delta + c5*G + c6*zhulak + c7*medFA + c8*medMiss
+# # loss_3 = mse + mse*c1*hausdorf_distance + c2*mse*phdk_distance - c3*gbeta*mse + c4*delta*mse + c6*zhulak + c7*medFA + c8*medMiss
 
-loss_1 = mse + mse*cHaus*hausdorf_distance
-loss_2 = mse + mse*phdk_distance
-loss_3 = mse + mse*cGbeta*gbeta*-1
-loss_4 = mse + mse*cDelta*delta
-loss_5 = mse + (far-pod)*mse*cCSI + mse*cHaus*hausdorf_distance + mse*cPHDK*phdk_distance - mse*cGbeta*gbeta + mse*cDelta*delta + cG*G + cZhu*zhulak + cFA*medFA + cMiss*medMiss
+# loss_1 = mse + mse*cHaus*hausdorf_distance
+# loss_2 = mse + mse*phdk_distance
+# loss_3 = mse + mse*cGbeta*gbeta*-1
+# loss_4 = mse + mse*cDelta*delta
+# loss_5 = mse + (far-pod)*mse*cCSI + mse*cHaus*hausdorf_distance + mse*cPHDK*phdk_distance - mse*cGbeta*gbeta + mse*cDelta*delta + cG*G + cZhu*zhulak + cFA*medFA + cMiss*medMiss
 
-loss_dict = {'Haus': [loss_1], 'PHDK': [loss_2], 'Gbeta': [loss_3], 'Delta': [loss_4], 'All': [loss_5]}
-# coeficients_dict = {'c0': [c0], 'c1': [c1], 'c2': [c2], 'c3': [c3], 'c4': [c4], 'c5': [c5], 'c6': [c6], 'c7': [c7], 'c8': [c8]}
-coefficients_dict = {'cCSI': [cCSI], 'cHaus': [cHaus], 'cPHDK': [cPHDK], 'cGbeta': [cGbeta], 'cDelta': [cDelta], 'cG': [cG], 'cZhu': [cZhu], 'cFA': [cFA], 'cMiss': [cMiss]}
+# loss_dict = {'Haus': [loss_1], 'PHDK': [loss_2], 'Gbeta': [loss_3], 'Delta': [loss_4], 'All': [loss_5]}
+# # coeficients_dict = {'c0': [c0], 'c1': [c1], 'c2': [c2], 'c3': [c3], 'c4': [c4], 'c5': [c5], 'c6': [c6], 'c7': [c7], 'c8': [c8]}
+# coefficients_dict = {'cCSI': [cCSI], 'cHaus': [cHaus], 'cPHDK': [cPHDK], 'cGbeta': [cGbeta], 'cDelta': [cDelta], 'cG': [cG], 'cZhu': [cZhu], 'cFA': [cFA], 'cMiss': [cMiss]}
 
-df = pd.DataFrame(metrics_dict)
-df2 = pd.DataFrame(metrics_dict_2)
-df3 = pd.DataFrame(loss_dict)
-df4 = pd.DataFrame(coefficients_dict)
-st.table(df3)
-st.table(df)
-st.table(df2)
+# df = pd.DataFrame(metrics_dict)
+# df2 = pd.DataFrame(metrics_dict_2)
+# df3 = pd.DataFrame(loss_dict)
+# df4 = pd.DataFrame(coefficients_dict)
+# st.table(df3)
+# st.table(df)
+# st.table(df2)
 
-st.table(df4)
+# st.table(df4)
 
